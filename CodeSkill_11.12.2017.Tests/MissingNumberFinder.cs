6132c5993d06479dba0df5db33ac98b2a7263ca6
DESKTOP-USAOD01\KrisJ <krzjurczyk@gmail.com>
2017-12-11 21:45:55 +0100

    bug fixed

---
+++ CodeSkill_11.12.2017.Tests/MissingNumberFinder.cs
@@ -0,0 +1,34 @@
+﻿using CodeSkill_MissingNumber;
+using NUnit.Framework;
+using System;
+
+namespace CodeSkill_11._12._2017.Tests
+{
+    [TestFixture]
+    public class MissingNumberFinder
+    {
+        [Test]
+        public void FirstMissingNumber_WithMissingNumbers_UpByOne()
+        {
+            var w = new int[] { -3, -2, 2, 4 };
+            var a = new CodeSkill_MissingNumber.MissingNumberFinder(w);
+            Assert.That(a.FirstMissingNumber, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void FirstMissingNumber_WithAllNumbers_UpByOne()
+        {
+            var w = new int[] { -3, -2, -1, 0, 1, 2, 3, 4 };
+            var a = new CodeSkill_MissingNumber.MissingNumberFinder(w);
+            Assert.That(a.FirstMissingNumber, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void FirstMissingNumber_WithEmptyArray_ThrowException()
+        {
+            var w = new int[] {};
+            var a = new CodeSkill_MissingNumber.MissingNumberFinder(w);
+            Assert.Throws<Exception>(() => a.FirstMissingNumber(), "Collection is empty, unspecified beginning of the collection");
+        }
+    }
+}
---
+++ CodeSkill_MissingNumber/MissingNumberFinder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSkill_MissingNumber
+{
+    public class MissingNumberFinder
+    {
+        private int[] _tab;
+        private int _result;
+        public MissingNumberFinder(int[] tab)
+        {
+            _tab = tab;
+        }
+
+        public int FirstMissingNumber()
+        {
+            if (_tab.Length == 0)
+            {
+                throw new Exception("Collection is empty, unspecified beginning of the collection");
+            }
+
+            else
+            {
+                for (int i = 0; i < _tab.Length - 1; i++)
+                {
+                    if (_tab[i + 1] - _tab[i] != 1)
+                    {
+                        _result = _tab[i] + 1;
+                        break;
+                    }
+                    else _result = _tab[i + 1] + 1;
+                }
+            }
+            return _result;
+        }
+    }
+}
